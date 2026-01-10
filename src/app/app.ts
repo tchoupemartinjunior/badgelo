988f16a329f7ee73d1d6043ecd0a013314447826
MJ_Tchoupe <tchoupemartinjunior@gmail.com>
2026-01-10 21:29:03 +0100

    create navbar component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from '@layout/footer/footer';
 import { Header } from "@layout/header/header";
+import { Navbar } from "@layout/navbar/navbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header,Footer],
+  imports: [RouterOutlet, Header, Footer, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
