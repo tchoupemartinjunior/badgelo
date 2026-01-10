d404ac2f64c606def6223f423fa5ce9b3fb56ea4
MJ_Tchoupe <tchoupemartinjunior@gmail.com>
2026-01-10 12:41:32 +0100

    define app base structure

---
+++ src/app/layout/footer/footer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bdge-footer',
+  imports: [],
+  templateUrl: './footer.html',
+  styleUrl: './footer.scss',
+})
+export class Footer {
+
+}
