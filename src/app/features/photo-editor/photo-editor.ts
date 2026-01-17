9de69a668a6fcc524689e7ad16dd37653ddb3666
MJ_Tchoupe <tchoupemartinjunior@gmail.com>
2026-01-17 17:52:47 +0100

    add raw download functionnality

--- src/app/features/photo-editor/photo-editor.ts
+++ src/app/features/photo-editor/photo-editor.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild,ElementRef } from '@angular/core';
 import { FileUploadService } from '../../shared/services/file-upload.service';
 
 @Component({
@@ -9,5 +9,31 @@ import { FileUploadService } from '../../shared/services/file-upload.service';
 })
 export class PhotoEditor {
   fileUploadService = inject(FileUploadService);
+  watermarkText: string = '';
+  imageSrc: string|null = this.fileUploadService.previewUrl();
+
+  @ViewChild('canvas') canvas!: ElementRef<HTMLCanvasElement>;
+
+download() {
+  const canvas = this.canvas.nativeElement;
+  const ctx = canvas.getContext('2d')!;
+  const img = new Image();
+
+  img.src = this.imageSrc!;
+
+  img.onload = () => {
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    ctx.filter = 'brightness(120%) contrast(110%)';
+    ctx.rotate((5 * Math.PI) / 180);
+    ctx.drawImage(img, 0, 0);
+
+    const link = document.createElement('a');
+    link.download = 'edited-image.png';
+    link.href = canvas.toDataURL('image/png');
+    link.click();
+  };
+}
 
 }
