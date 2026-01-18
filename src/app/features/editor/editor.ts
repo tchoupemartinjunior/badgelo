dd8d30a0cc60d6b54260d8b8d8d9a4b8d022b34c
MJ_Tchoupe <tchoupemartinjunior@gmail.com>
2026-01-18 23:20:39 +0100

    add base edition tools

--- src/app/features/editor/editor.ts
+++ src/app/features/editor/editor.ts
@@ -1,4 +1,4 @@
-import { Component, inject, ViewChild, ElementRef } from '@angular/core';
+import { Component, inject, ViewChild, ElementRef, TemplateRef } from '@angular/core';
 import { FileUploadService } from '../../shared/services/file-upload.service';
 import { UploadButton } from "@shared/components/upload-button/upload-button";
 import { Preview } from "./preview/preview";
@@ -11,12 +11,24 @@ import { NgClass, NgTemplateOutlet } from '@angular/common';
   styleUrl: './editor.scss',
 })
 export class Editor {
+  @ViewChild('baseTemplate', { static: true }) baseTemplate!: TemplateRef<any>;
+  @ViewChild('filtersTemplate', { static: true }) filtersTemplate!: TemplateRef<any>;
+  @ViewChild('transformTemplate', { static: true }) transformTemplate!: TemplateRef<any>;
+
+
   fileUploadService = inject(FileUploadService);
   activeTab: string = 'photo';
+  openAccordion: string | null = null;
 
   tabs = [
     { key: 'photo', label: 'Edition Photo' },
     { key: 'badge', label: 'Badge' }
+  ];
+
+  accordions = [
+    { key: 'base', label: 'Réglages de base' },
+    { key: 'filters', label: 'Filtres' },
+    { key: 'transform', label: 'Transformations' }
   ];
 
 
@@ -24,10 +36,22 @@ export class Editor {
     this.fileUploadService.upload($event);
   }
 
+  getTemplate(key: string): TemplateRef<any> {
+    console.log('Getting template for key:', key);
+    console.log('Available templates:', {
+      base: this.baseTemplate,
+      filters: this.filtersTemplate,
+      transform: this.transformTemplate
+    });
+    switch (key) {
+      case 'base': return this.baseTemplate;
+      case 'filters': return this.filtersTemplate;
+      case 'transform': return this.transformTemplate;
+      default: throw new Error('Template not found');
+    }
+  }
 
 
-  watermarkText: string = 'HAPPY BIRTHDAY';
-  imageSrc: string | null = this.fileUploadService.previewUrl();
 
   @ViewChild('canvas') canvas!: ElementRef<HTMLCanvasElement>;
 
@@ -36,7 +60,7 @@ export class Editor {
     const ctx = canvas.getContext('2d')!;
     const img = new Image();
 
-    img.src = this.imageSrc!;
+    img.src = this.fileUploadService.previewUrl()!;
 
     img.onload = () => {
       canvas.width = img.width;
@@ -49,11 +73,6 @@ export class Editor {
       ctx.font = '120px serif';
       ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
       ctx.textAlign = 'center';
-
-      ctx.fillText(this.watermarkText, canvas.width / 2, canvas.height - 20);
-      // add blue background to text
-      ctx.fillRect((canvas.width / 2) - (ctx.measureText(this.watermarkText).width / 2), canvas.height - 140, ctx.measureText(this.watermarkText).width + 20, 100);
-      ctx.fillStyle = 'rgba(95, 95, 233, 0.5)';
 
       const link = document.createElement('a');
       link.download = 'edited-image.png';
