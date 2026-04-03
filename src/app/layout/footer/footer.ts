0ce487e28f9eb8e6a3b5411d42ff1cfbcf988c83
MJ_Tchoupe <tchoupemartinjunior@gmail.com>
2026-04-03 12:47:16 +0200

    add gif demos and logo

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'bdge-footer',
-  imports: [TranslateModule],
+  imports: [NgOptimizedImage, TranslateModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
